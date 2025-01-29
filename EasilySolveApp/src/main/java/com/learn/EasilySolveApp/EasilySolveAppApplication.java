e3654d93bf89c6e001718246fc3cecde5e5a0916
CJSanchez <cjsanchez604@gmail.com>
2025-01-29 15:28:53 +0800

    Setup db and springboot project structure

--- EasilySolveApp/src/main/java/com/learn/EasilySolveApp/EasilySolveAppApplication.java
+++ EasilySolveApp/src/main/java/com/learn/EasilySolveApp/EasilySolveAppApplication.java
@@ -2,8 +2,10 @@ package com.learn.EasilySolveApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.learn.EasilySolveApp.pojos.entities")
 public class EasilySolveAppApplication {
 
 	public static void main(String[] args) {
