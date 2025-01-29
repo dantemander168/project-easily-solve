6ffd3b090cbbc5435e68c98b5b4ff08c6d0a4ef2
CJSanchez <cjsanchez604@gmail.com>
2025-01-29 13:48:02 +0800

    Initialize repo

---
+++ EasilySolveApp/src/main/java/com/learn/EasilySolveApp/EasilySolveAppApplication.java
@@ -0,0 +1,13 @@
+package com.learn.EasilySolveApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasilySolveAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasilySolveAppApplication.class, args);
+	}
+
+}
